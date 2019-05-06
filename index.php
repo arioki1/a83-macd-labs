cb5bf684c09223209ef5b8c293da3f4821f9c2ad
Arioki Dev <yogainformatika@gmail.com>
2019-05-06 11:22:42 +0700

    test

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "yogadb.database.windows.net";
+    $user = "arioki1";
+    $pass = "Yoga54321";
+    $db = "submission1-yoga";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
